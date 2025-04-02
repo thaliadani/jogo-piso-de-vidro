c9c934dff491a60e20af3d086c5f122828cd0a54
thaliadani <thaliadani2@gmail.com>
2025-04-02 16:47:04 -0300

    melhora a lógica do jogo para validação de entrada do jogador e renomeia variáveis para maior clareza

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 function jogar() {
-  let rodada = 1;
-  while (rodada <= 3) {
-    console.log(`Rodada ${rodada}`);
+    let numeroDeRodadas = 1;
 
-    let escolhaJogador = prompt(
-      "Rodada " + rodada + ", escolha um piso de 1 a 3:"
-    );
-    let pisoQuebrado = Math.floor(Math.random() * 3 + 1);
+    while (numeroDeRodadas <= 3) {
+        console.log(`Rodada ${numeroDeRodadas}`);
 
-    if (escolhaJogador == pisoQuebrado) {
-        alert("Vidro quebrou!");
-        break;
-    }else {
-        alert("Vidro não quebrou! Proxima rodada!");
+        let escolhaJogador;
+        do {
+            escolhaJogador = parseInt(
+                prompt("Rodada " + numeroDeRodadas + ", escolha um piso de 1 a 3:")
+            );
+            if (isNaN(escolhaJogador) || escolhaJogador < 1 || escolhaJogador > 3) {
+                alert("Escolha inválida! Por favor, escolha um número entre 1 e 3.");
+            }
+        } while (isNaN(escolhaJogador) || escolhaJogador < 1 || escolhaJogador > 3);
+
+        let pisoQuebrado = Math.floor(Math.random() * 3 + 1);
+
+        if (escolhaJogador === pisoQuebrado) {
+            alert("Vidro quebrou!");
+            break;
+        } else {
+            alert("Vidro não quebrou! Próxima rodada!");
+        }
+
+        numeroDeRodadas++;
     }
-    
-    rodada++;
-  }
 
-  if (rodada > 3) {
-    alert("Ganhou! Parabéns!");
-  }
+    if (numeroDeRodadas > 3) {
+        alert("Ganhou! Parabéns!");
+    }
 }
