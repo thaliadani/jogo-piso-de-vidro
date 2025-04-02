35e1f7c6e33c2c8c4c220b0132ad97a4bda141cb
thaliadani <thaliadani2@gmail.com>
2025-04-02 16:33:52 -0300

    adiciona lógica de jogo com rodadas e condições de vitória

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function jogar() {
+  let rodada = 1;
+  while (rodada <= 3) {
+    console.log(`Rodada ${rodada}`);
+
+    let escolhaJogador = prompt(
+      "Rodada " + rodada + ", escolha um piso de 1 a 3:"
+    );
+    let pisoQuebrado = Math.floor(Math.random() * 3 + 1);
+
+    if (escolhaJogador == pisoQuebrado) {
+        alert("Vidro quebrou!");
+        break;
+    }else {
+        alert("Vidro não quebrou! Proxima rodada!");
+    }
     
+    rodada++;
+  }
+
+  if (rodada > 3) {
+    alert("Ganhou! Parabéns!");
+  }
 }
